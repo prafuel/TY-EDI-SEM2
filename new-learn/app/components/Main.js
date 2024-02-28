cd2aad38ceb3a5921de0d3ae3dd355a83ec2459d
bhushansonsale09 <bhushanofficial0908@gmail.com>
2024-02-28 23:00:45 +0530

    added

--- new-learn/app/components/Main.js
+++ new-learn/app/components/Main.js
@@ -1,12 +1,15 @@
-import React from 'react'
+// pages/index.js
+
+import React from 'react';
 
 const Main = () => {
   return (
-    <div>
-        Main
-        <a href="/features" className="text-red-700 text-3xl">to Feature</a>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br">
+      <h1 className="text-4xl font-bold text-center text-white mb-4">Welcome to GenCraft.ai</h1>
+      <p className="text-lg text-center text-white mb-8">Welcome to the World of Generative AI. Let's flaunt through our exciting tools.</p>
+      <button className="bg-transparent border border-white text-white hover:bg-white hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-6 py-3">Get Started</button>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- new-learn/app/page.js
+++ new-learn/app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
@@ -5,7 +6,7 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
 
-    <main className="container min-w-full min-h-screen overflow-auto ">
+    <main className="min-w-full min-h-screen overflow-auto bg-gradient-to-br from-gray-800 to-blue-400">
       <Navbar/>
       <Main />
 
